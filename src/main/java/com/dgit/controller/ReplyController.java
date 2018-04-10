f5681f912e382724b13af9a7b530579f43ab5758
Mosquito(A) <skykim10908@naver.com>
2018-04-10 17:14:41 +0900

    reple update

--- src/main/java/com/dgit/controller/ReplyController.java
+++ src/main/java/com/dgit/controller/ReplyController.java
@@ -1,32 +1,56 @@
 package com.dgit.controller;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dgit.domain.Criteria;
+import com.dgit.domain.PageMaker;
+import com.dgit.domain.ReplyVO;
+import com.dgit.service.ReplyService;
 
 @RestController
 @RequestMapping("read")
 public class ReplyController {
 	private static final Logger logger = LoggerFactory.getLogger(ReplyController.class);
 	
-	@RequestMapping(value="/{}/{}", method = RequestMethod.GET)
-	public ResponseEntity<Map<String, Object>> getReply(){
+	@Autowired
+	private ReplyService replyService;
+	
+	@RequestMapping(value="/{boardNum}/{page}", method = RequestMethod.GET)
+	public ResponseEntity<Map<String, Object>> getReply(@PathVariable("boardNum") int boardNum, @PathVariable int page){
 		ResponseEntity<Map<String, Object>> entity = null;
+		Criteria cri = new Criteria();
+		cri.setPage(page);
+		cri.setPerPageNum(5);
 		
 		try{
+			List<ReplyVO> reples = replyService.selectReplyByBoardNum(boardNum, cri);
 			
+			PageMaker pageMaker = new PageMaker();
+			pageMaker.setCri(cri);
+			pageMaker.setTotalCount(replyService.selectReplyCount(boardNum));
+			
+			HashMap<String, Object> map = new HashMap<>();
+			map.put("reples", reples);
+			map.put("pageMaker", pageMaker);
+			
+			entity = new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
 			
 		}catch(Exception e){
-			
+			entity = new ResponseEntity<Map<String, Object>>(HttpStatus.BAD_REQUEST);
 		}
-		
-		
-		
+				
 		return entity;
 	}
 	
